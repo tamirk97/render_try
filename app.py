42055dfdf2775322b3e19d3c465daaab6c290c5f
tamir katz <tamirk97@gmail.com>
2024-07-08 12:16:50 +0300

    Updated index.html and app.py with Axios integration

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def hello_world():
-    return {"Hello:World"}
+    return {"message": "Hello, World!"}
 
 if __name__ == '__main__':
     app.run(debug=True)
